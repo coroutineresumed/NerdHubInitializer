e24363ffc3d14e15c2c3dbf290cee61f15c465ae
shockwinning <89555942+shockwinning@users.noreply.github.com>
2022-10-16 13:22:54 -0700

    Create Initializer.lua

---
+++ Initializer.lua
@@ -0,0 +1,21 @@
+local ids = {
+    ["NST"] = 6008108575,
+    ["Mortem Metallum"] = 4169490976
+}
+
+local KickPlayer = function(msg)
+    game.GetService(game, "Players").LocalPlayer.Kick(game.GetService(game, "Players").LocalPlayer, msg)
+end
+
+if game.PlaceId == ids["NST"] then
+    local status, script = pcall(game.HttpGet, game, 'https://raw.githubusercontent.com/coroutineresumed/NerdHubInitializer/main/NST.Init')
+    if status then
+        loadstring(script)()
+    else
+        KickPlayer("failed to require loadstring | ERROR 1") 
+    end
+elseif game.PlaceId == ids["Mortem Metallum"] then
+    -- not yet obfuscated / finished
+else
+    game.GetService(game, "Players").LocalPlayer.Kick(game.GetService(game, "Players").LocalPlayer, "Game is not supported.")
+end
