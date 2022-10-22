df486e115ff319c4e6d51f2d059522e53b9e23d5
coroutineresumed <115513722+coroutineresumed@users.noreply.github.com>
2022-10-22 13:28:18 -0700

    Update Initializer.lua

--- Initializer.lua
+++ Initializer.lua
@@ -1,6 +1,8 @@
 local ids = {
     ["NST"] = 6008108575,
-    ["Mortem Metallum"] = 4169490976
+    ["Mortem Metallum"] = {Alpha = 4169490976, Beta = 8986335348},
+    ["Assassin"] = {Classic = 379614936, FreePlay = 5006801542, Pro = 860428890},
+    ["Combat Warriors"] = 4282985734
 }
 
 local KickPlayer = function(msg)
@@ -12,10 +14,29 @@ if game.PlaceId == ids["NST"] then
     if status then
         loadstring(script)()
     else
-        KickPlayer("failed to require loadstring | ERROR 1") 
+        KickPlayer("failed to require loadstring | does your exploit support loadstring?")
     end
-elseif game.PlaceId == ids["Mortem Metallum"] then
-    -- not yet obfuscated / finished
+elseif table.find(ids["Mortem Metallum"], game.PlaceId) then
+    local status, script = pcall(game.HttpGet, game, 'https://raw.githubusercontent.com/coroutineresumed/NerdHubInitializer/main/Mortem_Metallum.lua')
+    if status then
+        loadstring(script)()
+    else
+        KickPlayer("failed to require loadstring | does your exploit support loadstring?")
+    end
+elseif table.find(ids["Assassin"], game.PlaceId) or game.PlaceId == ids['Assassin'].Classic or game.PlaceId == ids['Assassin'].FreePlay or game.PlaceId == ids['Assassin'].Pro then
+    local status, script = pcall(game.HttpGet, game, 'https://raw.githubusercontent.com/coroutineresumed/NerdHubInitializer/main/Assassin')
+    if status then
+        loadstring(script)()
+    else
+        KickPlayer("failed to require loadstring | does your exploit support loadstring?")
+    end
+elseif game.PlaceId == ids["Combat Warriors"] then
+    local status, script = pcall(game.HttpGet, game, 'https://raw.githubusercontent.com/coroutineresumed/NerdHubInitializer/main/Combat_Warriors.lua')
+    if status then
+        loadstring(script)()
+    else
+        KickPlayer("failed to require loadstring | does your exploit support loadstring?")
+    end
 else
     game.GetService(game, "Players").LocalPlayer.Kick(game.GetService(game, "Players").LocalPlayer, "Game is not supported.")
 end
