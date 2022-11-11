217027e2f7bc4f0992bf660952bc125c06837d6b
coroutineresumed <115513722+coroutineresumed@users.noreply.github.com>
2022-11-11 13:55:08 -0800

    Update Initializer.lua

--- Initializer.lua
+++ Initializer.lua
@@ -2,41 +2,49 @@ local ids = {
     ["NST"] = 6008108575,
     ["Mortem Metallum"] = {Alpha = 4169490976, Beta = 8986335348},
     ["Assassin"] = {Classic = 379614936, FreePlay = 5006801542, Pro = 860428890},
-    ["Combat Warriors"] = 4282985734
+    ["Murder Mystery 2"] = 142823291,
+    ["Revenger"] = 7021728144,
 }
 
-local KickPlayer = function(msg)
+local Disconnect = function(msg)
     game.GetService(game, "Players").LocalPlayer.Kick(game.GetService(game, "Players").LocalPlayer, msg)
 end
 
+local LoadScript = function(scr)
+    local executor = identifyexecutor()
+    if not executor then
+        Disconnect("failed to identify executor")
+    end
+
+    if tostring(executor):lower():find("synapse") then
+        local status, script = pcall(game.HttpGet, game, scr)
+        if status then
+            loadstring(script)()
+        else
+            Disconnect("failed to require loadstring")
+        end
+    elseif tostring(executor):lower():find("scriptware") then
+        local status, script = pcall(http.request, {Url = scr, Method = "GET"})
+        if status then
+            loadstring(script.Body)()
+        else
+            Disconnect("failed to require loadstring")
+        end
+    end
+end
+
 if game.PlaceId == ids["NST"] then
-    local status, script = pcall(game.HttpGet, game, 'https://raw.githubusercontent.com/coroutineresumed/NerdHubInitializer/main/NST.lua')
-    if status then
-        loadstring(script)()
-    else
-        KickPlayer("failed to require loadstring | does your exploit support loadstring?")
-    end
-elseif table.find(ids["Mortem Metallum"], game.PlaceId) or game.PlaceId == ids["Mortem Metallum"].Alpha or game.PlaceId == ids["Mortem Metallum"].Beta then
-    local status, script = pcall(game.HttpGet, game, 'https://raw.githubusercontent.com/coroutineresumed/NerdHubInitializer/main/Mortem_Metallum.lua')
-    if status then
-        loadstring(script)()
-    else
-        KickPlayer("failed to require loadstring | does your exploit support loadstring?")
-    end
-elseif table.find(ids["Assassin"], game.PlaceId) or game.PlaceId == ids['Assassin'].Classic or game.PlaceId == ids['Assassin'].FreePlay or game.PlaceId == ids['Assassin'].Pro then
-    local status, script = pcall(game.HttpGet, game, 'https://raw.githubusercontent.com/coroutineresumed/NerdHubInitializer/main/Assassin')
-    if status then
-        loadstring(script)()
-    else
-        KickPlayer("failed to require loadstring | does your exploit support loadstring?")
-    end
-elseif game.PlaceId == ids["Combat Warriors"] then
-    local status, script = pcall(game.HttpGet, game, 'https://raw.githubusercontent.com/coroutineresumed/NerdHubInitializer/main/Combat_Warriors.lua')
-    if status then
-        loadstring(script)()
-    else
-        KickPlayer("failed to require loadstring | does your exploit support loadstring?")
-    end
+    LoadScript("https://raw.githubusercontent.com/coroutineresumed/NerdHubInitializer/main/NST.lua")
+elseif game.PlaceId == ids["Mortem Metallum"].Alpha or game.PlaceId == ids["Mortem Metallum"].Beta then
+    LoadScript("https://raw.githubusercontent.com/coroutineresumed/NerdHubInitializer/main/Mortem_Metallum.lua")
+elseif game.PlaceId == ids['Assassin'].Classic or game.PlaceId == ids['Assassin'].FreePlay or game.PlaceId == ids['Assassin'].Pro then
+    LoadScript("https://raw.githubusercontent.com/coroutineresumed/NerdHubInitializer/main/Assassin.lua")
+--elseif game.PlaceId == ids["Combat Warriors"] then
+    --LoadScript("https://raw.githubusercontent.com/coroutineresumed/NerdHubInitializer/main/Combat_Warriors.lua")
+elseif game.PlaceId == ids["Murder Mystery 2"] then
+    LoadScript("https://raw.githubusercontent.com/coroutineresumed/NerdHubInitializer/main/MM2.lua")
+elseif game.PlaceId == ids["Revenger"] then
+    LoadScript("https://raw.githubusercontent.com/coroutineresumed/NerdHubInitializer/main/Revengers_Online.lua")
 else
-    game.GetService(game, "Players").LocalPlayer.Kick(game.GetService(game, "Players").LocalPlayer, "Game is not supported.")
+    Disconnect("game is unsupported")
 end
